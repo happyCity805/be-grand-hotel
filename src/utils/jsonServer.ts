d769be34e94eebc19cc940ca7a796c38d94d2b39
Happy City <happycity805@gmail.com>
2025-06-18 14:56:03 +0700

    delete json-server

--- src/utils/jsonServer.ts
+++ src/utils/jsonServer.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const jsonApi = axios.create({
-  baseURL: process.env.JSON_SERVER_URL || 'http://localhost:3001',
+  baseURL:
+    process.env.JSON_SERVER_URL ||
+    'http://localhost:3001' ||
+    'https://my-json-server.typicode.com/happyCity805/be-grand-hotel',
 });
